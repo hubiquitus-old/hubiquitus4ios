8def0fbe912238fadb62bf06c36d9eb75d30ccdb
Nadim <nadim.boustani@gmail.com>
2012-07-09 14:07:43 +0200

    Updated SimpleClient Example

--- examples/SimpleClient/ConnectionController.h
+++ examples/SimpleClient/ConnectionController.h
@@ -1,14 +1,29 @@
-//
-//  ConnectionController.h
-//  SimpleClient
-//
-//  Created by Novedia Agency on 06/07/12.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
-//
+/*
+ * Copyright (c) Novedia Group 2012.
+ *
+ *     This file is part of Hubiquitus.
+ *
+ *     Hubiquitus is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     Hubiquitus is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with Hubiquitus.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #import <UIKit/UIKit.h>
 
-@interface ConnectionController : UIViewController
+@interface ConnectionController : UIViewController<UITextFieldDelegate>
+
+@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
+
+@property (weak, nonatomic) UITextField *activeField;
 
 @property (weak, nonatomic) IBOutlet UITextField *publisher;
 @property (weak, nonatomic) IBOutlet UITextField *password;
@@ -22,11 +37,11 @@
 @property (weak, nonatomic) IBOutlet UILabel *errorMsg;
 
 - (IBAction)hideKeyboard:(id)sender;
+- (void)registerForKeyboardNotifications;
+- (void)keyboardWasShown:(NSNotification*)aNotification;
+- (void)keyboardWillBeHidden:(NSNotification*)aNotification;
 
 - (IBAction)connect:(id)sender;
 - (IBAction)disconnect:(id)sender;
-
-
-
 
 @end
--- examples/SimpleClient/ConnectionController.m
+++ examples/SimpleClient/ConnectionController.m
@@ -1,10 +1,21 @@
-//
-//  ConnectionController.m
-//  SimpleClient
-//
-//  Created by Novedia Agency on 06/07/12.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
-//
+/*
+ * Copyright (c) Novedia Group 2012.
+ *
+ *     This file is part of Hubiquitus.
+ *
+ *     Hubiquitus is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     Hubiquitus is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with Hubiquitus.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #import "ConnectionController.h"
 
@@ -13,6 +24,7 @@
 @end
 
 @implementation ConnectionController
+@synthesize scrollView;
 @synthesize publisher;
 @synthesize password;
 @synthesize endpoints;
@@ -21,6 +33,7 @@
 @synthesize connStatus;
 @synthesize errorCode;
 @synthesize errorMsg;
+@synthesize activeField;
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -34,6 +47,7 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    [self registerForKeyboardNotifications];
 	// Do any additional setup after loading the view.
 }
 
@@ -47,6 +61,8 @@
     [self setConnStatus:nil];
     [self setErrorCode:nil];
     [self setErrorMsg:nil];
+    [self setScrollView:nil];
+    [self setActiveField:nil];
     [super viewDidUnload];
     // Release any retained subviews of the main view.
 }
@@ -57,11 +73,58 @@
 }
 
 - (IBAction)hideKeyboard:(id)sender {
-    [publisher resignFirstResponder];
-    [password resignFirstResponder];
-    [endpoints resignFirstResponder];
-    [serverHost resignFirstResponder];
-    [serverPort resignFirstResponder];
+    [activeField resignFirstResponder];
+}
+
+// Call this method somewhere in your view controller setup code.
+- (void)registerForKeyboardNotifications
+{
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(keyboardWasShown:)
+                                                 name:UIKeyboardDidShowNotification object:nil];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(keyboardWillBeHidden:)
+                                                 name:UIKeyboardWillHideNotification object:nil];
+    
+}
+
+// Called when the UIKeyboardDidShowNotification is sent.
+- (void)keyboardWasShown:(NSNotification*)aNotification
+{
+    NSDictionary* info = [aNotification userInfo];
+    CGSize kbSize = [[info objectForKey:UIKeyboardFrameBeginUserInfoKey] CGRectValue].size;
+    
+    UIEdgeInsets contentInsets = UIEdgeInsetsMake(0.0, 0.0, kbSize.height + scrollView.bounds.size.height, 0.0);
+    scrollView.contentInset = contentInsets;
+    scrollView.scrollIndicatorInsets = contentInsets;
+    
+    // If active text field is hidden by keyboard, scroll it so it's visible
+    // Your application might not need or want this behavior.
+    CGRect aRect = self.view.frame;
+    aRect.size.height -= kbSize.height;
+    if (!CGRectContainsPoint(aRect, activeField.frame.origin) ) {
+        CGPoint scrollPoint = CGPointMake(0.0, activeField.frame.origin.y + activeField.frame.size.height - aRect.size.height);
+        [scrollView setContentOffset:scrollPoint animated:YES];
+    }
+}
+
+// Called when the UIKeyboardWillHideNotification is sent
+- (void)keyboardWillBeHidden:(NSNotification*)aNotification
+{
+    UIEdgeInsets contentInsets = UIEdgeInsetsZero;
+    scrollView.contentInset = contentInsets;
+    scrollView.scrollIndicatorInsets = contentInsets;
+}
+
+- (void)textFieldDidBeginEditing:(UITextField *)textField
+{
+    activeField = textField;
+}
+
+- (void)textFieldDidEndEditing:(UITextField *)textField
+{
+    activeField = nil;
 }
 
 - (IBAction)connect:(id)sender {
@@ -69,4 +132,5 @@
 
 - (IBAction)disconnect:(id)sender {
 }
+
 @end
