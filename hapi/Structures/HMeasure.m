621b3bf070dcd9a990a602b08831e3e191f1e2c5
Nadim <nadim.boustani@gmail.com>
2012-09-16 21:09:30 +0200

    Added hAck, hMeasure, hAlert, hConvState

---
+++ hapi/Structures/HMeasure.m
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) Novedia Group 2012.
+ *
+ *     This file is part of Hubiquitus.
+ *
+ *     Hubiquitus is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     Hubiquitus is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with Hubiquitus.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#import "HMeasure.h"
+#import "HNativeObjectsCategories.h"
+
+/**
+ * @version 0.5.0
+ * HMeasure
+ */
+
+#if ! __has_feature(objc_arc)
+#warning This file must be compiled with ARC. Use -fobjc-arc flag (or convert project to ARC).
+#endif
+
+@implementation HMeasure
+
+/**
+ * unit of the measure
+ */
+- (NSString*)unit {
+    return [self objectForKey:@"unit" withClass:[NSString class]];
+}
+
+- (void)setUnit:(NSString*)unit {
+    [self setObject:unit forKey:@"unit"];
+}
+
+/**
+ * value of the measure
+ */
+- (NSString*)value {
+    return [self objectForKey:@"value" withClass:[NSString class]];
+}
+
+- (void)setValue:(NSString*)value {
+    [self setObject:value forKey:@"value"];
+}
+
+@end
